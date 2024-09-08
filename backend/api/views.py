396a6810553f830528b0b396446e4d66ba98c024
Hunduma <hundumaaguutamaa@gmail.com>
2024-09-08 06:00:53 +0300

    serializes and views added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import ITTeam, RequestService, UserRequest
+from .serializer import ITTeamSerializer, RequestServiceSerializer, UserRequestSerializer
+
