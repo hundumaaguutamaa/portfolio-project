38de472fe4c97362b2d695e083232a6adce31592
Hunduma <hundumaaguutamaa@gmail.com>
2024-09-08 08:21:36 +0300

    frontend src added

---
+++ frontend/src/app.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import './main.css'
+
+const App = () => {
+  
+    return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
