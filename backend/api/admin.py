078c2bf900c32a47c636d4e0e41eab29a4906ace
Hunduma <hundumaaguutamaa@gmail.com>
2024-09-08 04:14:26 +0300

    models registered to admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ITTeam, UserRequest, RequestService
 
-# Register your models here.
+# Register your models to manage through admin interface.
+
+admin.site.register(ITTeam)
+admin.site.register(UserRequest)
+admin.site.register(RequestService)
