7fadd587a2a7e6e020e936fa9c5151fa78b525bf
Hunduma <hundumaaguutamaa@gmail.com>
2024-09-17 09:55:25 +0300

    Added frontend footer component

--- frontend/src/views/posts.vue
+++ frontend/src/views/posts.vue
@@ -2,19 +2,29 @@
   <div class="posts">
     <Header /> <!-- Using the Header component -->
     <Navbar /> <!-- Using the Navbar component -->
+   
     <h1> Hellooo Amanuel</h1>
+ 
+      <h2> Helen Dispatching today.</h2>
+  <p>
+      ALX Africa 
+
+  </p>
   </div>
+  <Footer /> <!-- Using the Navbar component -->
 </template>
 
 <script>
 import Header from '../components/Header.vue';
 import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
   name: 'Posts',
   components: {
     Header,
-    Navbar
+    Navbar,
+    Footer
   },
 }
 </script>
