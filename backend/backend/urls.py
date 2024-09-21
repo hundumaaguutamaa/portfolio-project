f81d9abd15eff4a4fa0152671488ec43eef8411d
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-09-21 05:29:58 +0300

    Installed djangorestframework-simplejwt for authentication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api import views  # Update with your app's views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 router = DefaultRouter()
 path('api/search/', views.SearchView.as_view()),
 router.register(r'teams', views.ITTeamViewSet)
@@ -14,4 +19,9 @@ urlpatterns = [
     path('api/search/', views.SearchView.as_view()),
     path('api/', include(router.urls)),  # Add your API routes
     path('', include(router.urls)),  # Redirect root to API
+
+     # JWT login and refresh token endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 ]
